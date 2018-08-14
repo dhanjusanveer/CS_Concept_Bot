c2672ed2b05a17e6e29eac303a9bbe8b33ae4be6
dhanjusanveer <42321064+dhanjusanveer@users.noreply.github.com>
2018-08-14 13:29:33 -0400

    Add second intent

--- index.js
+++ index.js
@@ -16,23 +16,28 @@ restService.use(bodyParser.json());
 let servicePrice = {'wireless': 40, 'internet':50, 'home phone': 20};
 
 restService.post("/inquiry", function(req, res) {
-  var text = req.body.queryResult.parameters.Service
   
-  let price = 0;
-  price = servicePrice[text];
-  
-  text = 'We offer ' + text + ' for only $' + price;
-  return res.json({
-    fulfillmentText: text
-  });
-});
+  var intent = req.body.queryResult.intent.displayName;
+ 
+  if (intent == "Inquiry") {
+    var text = req.body.queryResult.parameters.Service;
+    let price = 0;
+    price = servicePrice[text];
 
-restService.post("/echo", function(req, res) {
-  var text = req.body.queryResult.parameters.givenname
+    text = 'We offer ' + text + ' for only $' + price;
+    return res.json({
+      fulfillmentText: text
+    });
+  };
   
-  return res.json({
+  
+  if (intent == "Echo") {
+    var text = req.body.queryResult.parameters.givenname
+    return res.json({
     fulfillmentText: text
-  });
+   });
+  };
+ 
 });
 
 restService.listen(process.env.PORT || 8000, function() {
